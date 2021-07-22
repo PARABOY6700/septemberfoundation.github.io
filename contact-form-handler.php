2e5acb6f5930f6aa585d4b9e1b5fb056c79929b0
Abhishek Raj <87658393+PARABOY6700@users.noreply.github.com>
2021-07-22 05:39:38 +0530

    Add files via upload

---
+++ contact-form-handler.php
@@ -0,0 +1,13 @@
+<?php
+  $name = $_Post['name'];
+  $visitor_email = $_Post['email'];
+  $message = $_Post['message'];
+
+  $email_form ='rabhishek427@gmail.com';
+
+  $email_subject = 'new subscriber';
+   $email_body
+
+
+
+?>
